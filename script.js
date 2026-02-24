979a241c71f235ea3960b50f3f0b9d24b2ca1b1e
solusikasih <solusikasihsdnbhd@gmail.com>
2026-02-24 16:21:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const contactBtn = document.getElementById('cta-btn');
-
     contactBtn.addEventListener('click', () => {
-        // Smooth scroll to the contact section
-        document.getElementById('contact').scrollIntoView({ 
-            behavior: 'smooth' 
-        });
+        document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
     });
 });
